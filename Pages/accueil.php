3afd7c2520feb7f1697b150afe9d0f0069033a1e
Arthur <Charbonnel11065@jbdelasalle.com>
2020-05-15 09:44:35 +0200

    html css

--- Pages/accueil.php
+++ Pages/accueil.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE HTML>
 <html lang="fr">
     
@@ -6,9 +7,46 @@
     </head>
     
     <body>
+        
         <div class="grid">
             <div class="containerTaille">
+                <div class="containerTaille-top">
+                    Taille M
+                </div>
+                <div class="containerTaille-bottom">
+                    <img class="image" src="Photos/test.png" alt>
+                </div>
+                <div class="containerTaille-desc">
+                    M : une viande et une sauce au choix → 5€
+                    
+                </div>
                 
+            </div>
+            <div class="containerTaille">
+                <div class="containerTaille-top">
+                    Taille L
+                </div>
+                <div class="containerTaille-bottom">
+                    <img class="image" src="Photos/test.png" alt>
+                </div>
+                <div class="containerTaille-desc">
+                  L : deux viandes et deux sauces maximum → 7€
+                    
+                </div>
+               
+                
+            </div>
+            <div class="containerTaille">
+                <div class="containerTaille-top">
+                    Taille XL
+                </div>
+                <div class="containerTaille-bottom">
+                    <img class="image" src="Photos/test.png" alt>
+                </div>
+                <div class="containerTaille-desc">
+                    XL : trois viandes et deux sauces maximum → 9€
+                    
+                </div>
                 
             </div>
         </div>
